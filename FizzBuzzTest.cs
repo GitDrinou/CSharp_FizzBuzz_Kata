fdbff48d3163e1fb1668b537a0fa216cc7981979
sandrine Brival <sandrine.brival@octo.com>
2024-08-07 09:14:49 +0200

    Version 0.0

---
+++ FizzBuzzTest.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.InteropServices.JavaScript;
+using NFluent;
+using NUnit.Framework;
+namespace Kata_FizzBuzz;
+
+public class FizzBuzzTest{
+        [Test]
+        public void ShouldReturn1WhenGivenNumber1()
+        {
+            var fizzBuzz = new FizzBuzz();
+            var actual = fizzBuzz.Generate(1);
+            Check.That(actual).IsEqualTo("1");
+        }
+
+        [Test]
+        public void ShouldReturn2WhenGivenNumber2()
+        {
+            var fizzBuzz = new FizzBuzz();
+            var actual = fizzBuzz.Generate(2);
+            Check.That(actual).IsEqualTo("2");
+        }
+        
+        
+        
+        public class FizzBuzz
+         {
+             public string Generate(int number)
+             {
+                 return number.ToString();
+             }
+         }
+}
+
+
